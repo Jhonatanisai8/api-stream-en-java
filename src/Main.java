5c2d839f63e063835b7d8bf8b6203570a137579f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-20 21:43:02 -0500

    ejemplo 01 del uso de streams retornando una lista

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,5 +13,19 @@ public class Main {
                 .filter(s -> s.contains("J"))
                 //3. Operacion terminal
                 .forEach(System.out::println);
+        ejemplo01();
     }
+
+    public static void ejemplo01() {
+        System.out.println("Ejemplo 01 del uso de Streams");
+        List<String> nombres = new ArrayList<>(List.of("Java", "Python", "C#", "Kotlin", "JavaScript"));
+        //en este caso vamos a obtener una lista de resultados
+        List<String> resultado = nombres.stream()
+                .filter(s -> s.contains("J")) // filtrará los elementos que contengan la J
+                .toList(); // lo convierte a una lista
+        //mostramos
+        resultado.forEach(System.out::println);
+        System.out.println();
+    }
+
 }
